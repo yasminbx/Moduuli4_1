ab39f6b1f9261585da7c06eaacb246e34b1ca594
Zehra Ghali <Zehrag@metropolia.fi>
2024-04-16 23:39:54 +0300

    Tehtävä

---
+++ PalindromeChecker.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string to check if it's a palindrome:");
+        String input = scanner.nextLine();
+
+        String transformedInput = transformInput(input);
+
+        boolean isPalindrome = checkPalindrome(transformedInput);
+
+        if (isPalindrome) {
+            System.out.println("The transformed input is a palindrome.");
+        } else {
+            System.out.println("The transformed input is not a palindrome.");
+        }
+    }
+
+    public static String transformInput(String input) {
+        input = input.toLowerCase(); // Convert to lowercase
+        input = input.replaceAll("[^a-z]", "");
+        return input;
+    }
+
+    public static boolean checkPalindrome(String input) {
+        String reversed = reverseString(input);
+        return input.equals(reversed);
+    }
+
+    public static String reverseString(String input) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed.append(input.charAt(i));
+        }
+        return reversed.toString();
+    }
+}
